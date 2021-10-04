0458659c32ed932fc24ad3aa200d16e72c771983
sena <43688294+katesenalee@users.noreply.github.com>
2021-10-04 16:42:51 +0800

    Initial Commit

---
+++ playlists/playlistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  playlistsApp.swift
+//  playlists
+//
+//  Created by Sena Lee on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct playlistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
